7ee3632b1034c7a6d120373b72ffc12ff74be63c
NoobCoderAk <abidakbar0001@gmail.com>
2022-10-31 09:41:40 +0700

    add file java class Controller and HTML file

--- src/main/java/com/class_a/takehome/myController.java
+++ src/main/java/com/class_a/takehome/myController.java
@@ -4,13 +4,29 @@
  */
 package com.class_a.takehome;
 
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
  * @author ABID
  */
+@Controller
 public class myController {
     @RequestMapping("/input")
-    public String 
+    public String getInput(HttpServletRequest data, Model hiling){
+        String nama = data.getParameter("var_nama");
+        String lokasi = data.getParameter("var_lokasi");
+        String img = data.getParameter("var_img");
+        
+        hiling.addAttribute("nama",nama);
+        hiling.addAttribute("lokasi",lokasi);
+        hiling.addAttribute("img",img);
+        
+        return "rslt";
+    }
 }
