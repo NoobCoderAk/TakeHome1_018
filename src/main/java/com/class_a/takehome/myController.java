3206c35dfaf099c684fe05e1adc3b856f276ee40
NoobCoderAk <abidakbar0001@gmail.com>
2022-10-31 20:15:11 +0700

    modif controller

--- src/main/java/com/class_a/takehome/myController.java
+++ src/main/java/com/class_a/takehome/myController.java
@@ -5,11 +5,15 @@
 package com.class_a.takehome;
 
 
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -18,15 +22,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class myController {
     @RequestMapping("/input")
-    public String getInput(HttpServletRequest data, Model hiling){
-        String nama = data.getParameter("var_nama");
-        String lokasi = data.getParameter("var_lokasi");
-        String img = data.getParameter("var_img");
-        
-        hiling.addAttribute("nama",nama);
-        hiling.addAttribute("lokasi",lokasi);
-        hiling.addAttribute("img",img);
-        
+    public String getInput(
+            @RequestParam(value = "nama") String var_nama,
+            @RequestParam(value = "lokasi") String var_lokasi,
+            @RequestParam(value = "gambar") MultipartFile var_image,Model hiling
+        )throws IOException{
+        byte[] img = var_image.getBytes();
+        String base64Image = Base64.encodeBase64String(img);
+        String imglink = "data:image/*;base64,".concat(base64Image);
+        hiling.addAttribute("send_nama", var_nama);
+        hiling.addAttribute("send_lokasi", var_lokasi);
+        hiling.addAttribute("send_image", imglink);
         return "viewpage";
+        
     }
 }
