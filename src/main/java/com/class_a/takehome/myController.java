62f50b98c63e4bb3b37aadff0f94beef6fd8682d
NoobCoderAk <abidakbar0001@gmail.com>
2022-10-31 08:35:49 +0700

    add project

---
+++ src/main/java/com/class_a/takehome/myController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.class_a.takehome;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author ABID
+ */
+public class myController {
+    @RequestMapping("/input")
+    public String 
+}
